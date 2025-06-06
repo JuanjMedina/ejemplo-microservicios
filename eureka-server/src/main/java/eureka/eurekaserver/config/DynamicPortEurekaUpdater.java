c52b6415daf67b25451d8443821b8a7a60fe5cb6
Juan Medina <jmedinaguerrero847@gmail.com>
2025-06-05 22:29:24 -0500

    feat: add DynamicPortEurekaUpdater to update Eureka instance port dynamically on server initialization

---
+++ eureka-server/src/main/java/eureka/eurekaserver/config/DynamicPortEurekaUpdater.java
@@ -0,0 +1,33 @@
+package eureka.eurekaserver.config;
+
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DynamicPortEurekaUpdater implements ApplicationListener<WebServerInitializedEvent> {
+
+    private final EurekaInstanceConfigBean eurekaInstanceConfig;
+    private final EurekaClient eurekaClient;
+
+    public DynamicPortEurekaUpdater(EurekaInstanceConfigBean eurekaInstanceConfig, EurekaClient eurekaClient) {
+        this.eurekaInstanceConfig = eurekaInstanceConfig;
+        this.eurekaClient = eurekaClient;
+    }
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        int actualPort = event.getWebServer().getPort();
+        eurekaInstanceConfig.setNonSecurePort(actualPort);
+
+        // Forzar la actualización del estado en Eureka
+        eurekaClient.getApplicationInfoManager()
+                .refreshDataCenterInfoIfRequired();
+        eurekaClient.getApplicationInfoManager()
+                .setInstanceStatus(InstanceInfo.InstanceStatus.UP);
+    }
+}
